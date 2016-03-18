4b38f97791f7f639da3ec89248cf180c99a3eb3c
Alateon <dc.shoes@hotmail.fr>
2016-03-18 20:24:04 +0100

    Test header

--- js/main.js
+++ js/main.js
@@ -6,18 +6,9 @@ function getDaysInMonth(month,year)
 	return (new Date(Date.parse(((month%12)+1).toString() + "/01/" + year)-86400000)).getDate();
 }
 //renvoie le premier jour d'un moi d'une annee/*(1-12),9999*/ ex:getFirstDayInMonth(12,2000)
-function getFirstDayInMonth(whichMonth,Ofyear)
+function getJours(i)
 {
-	// var day
-
-	// if(whichMonth>12 || whichMonth<1 )
-	// {
-	// 	day='Undefined';
-	// 	whichMonth=1;
-	// }
-	// if(day!='Undefined')
-	// {
-	switch ((new Date(Ofyear+'-'+whichMonth+'-'+1)).getDay())
+	switch (i)
 	{
 		case 1:
 			day='Lundi';
@@ -41,7 +32,21 @@ function getFirstDayInMonth(whichMonth,Ofyear)
 			day='Dimanche';
 		break;
 	}
+	return day;
+}
+function getFirstDayInMonth(whichMonth,Ofyear)
+{
+	// var day
+
+	// if(whichMonth>12 || whichMonth<1 )
+	// {
+	// 	day='Undefined';
+	// 	whichMonth=1;
 	// }
+	// if(day!='Undefined')
+	// {
+	// }
+	getJours((new Date(Ofyear+'-'+whichMonth+'-'+1)).getDay());
 	return Day={nbDay:(new Date(Ofyear+'-'+whichMonth+'-'+1)).getDay(),nameDay:day};
 }
 //le tableau commence Lundi et fini le dimanche, il est statique et il comporte 6 lignes de jours en hauteur il comporte donc 42 jours
@@ -52,12 +57,15 @@ console.log(getFirstDayInMonth(03,2016).nameDay);
 //renvoie le nombre de jours dans une annee
 console.log(getDaysInMonth(03,2016));
 
-//longueur du tableu : 42
-console.log(42);
+function createMonthsWeekDay (i)
+{
+		var dayTitle=document.createElement('th');
+		dayTitle.setAttribute("id","calendrier-app-day-th-container-"+i)
+		$("#calendrier-app-day-tr-container-1")[0].appendChild(dayTitle);
+		$("#calendrier-app-day-th-container-"+i).text(getJours(i));
+}
 
-
-
-function createMonthsDays(nbFirdtDay,nbDaysInMonth,nbDaysInLastMonth)
+function createMonthsDays(nbFirdtDay,nbDaysInMonth,nbDaysInLastMonth)//refactoryser cette fonction separer le table et les tr calendarStructure
 {
 	var a=1;	
 	var b=1;
@@ -68,41 +76,46 @@ function createMonthsDays(nbFirdtDay,nbDaysInMonth,nbDaysInLastMonth)
 	table.setAttribute("class","calendrier-app-days-table-container");
 	$('.calendrier-app-container')[0].appendChild(table);
 
-	for(var i=0; i<6; i++)
+	for(var i=0; i<7; i++)
 	{
 		var tr = document.createElement('tr');
 		tr.setAttribute("id","calendrier-app-day-tr-container-"+b);
 		$('.calendrier-app-days-table-container')[0].appendChild(tr);
+
+
 		for(var y=0; y<7; y++)
 		{
-			var day = document.createElement('td');
-			day.setAttribute("id","calendrier-app-day-td-container-"+a);
-			day.setAttribute("class","calendrier-app-day-td-container");
-			day.style.width='100px';
-			day.style.height='100px';
-			$("#calendrier-app-day-tr-container-"+b)[0].appendChild(day);
 
-			if(a==nbFirdtDay || c>nbDaysInMonth)
+			if(b==1)
 			{
-				c=1;
-				d=1;
+				createMonthsWeekDay(a);
 			}
-			$("#calendrier-app-day-td-container-"+a).text(d);
+			else
+			{
+				var day = document.createElement('td');
+				day.setAttribute("id","calendrier-app-day-td-container-"+a);
+				day.setAttribute("class","calendrier-app-day-td-container");
+				day.style.width='100px';
+				day.style.height='100px';
+				$("#calendrier-app-day-tr-container-"+b)[0].appendChild(day);
+
+				if(a==nbFirdtDay || c>nbDaysInMonth)
+				{
+					c=1;
+					d=1;
+				}
+				$("#calendrier-app-day-td-container-"+a).text(d);
+			}
 			//console.log('c='+c+' | '+a+'=='+nbFirdtDay +'||'+ a+'=='+nbDaysInMonth);
-			a++;
+			if(b==1)
+			{
+			}
+			else{
+				a++;}
 			c++;
 			d++;
 		}
 		b++;
 	}
 }
-function createMonthsWeeks ()
-{
-	for(var i=0; i<7; i++)
-	{
-		var dayTitle=createElement('td');
-
-	}
-}
 createMonthsDays(getFirstDayInMonth(choosenMonth,2016).nbDay, getDaysInMonth(choosenMonth,2016), getDaysInMonth(choosenMonth-1,2016));
-createMonthsWeeks();
