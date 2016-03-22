52539df671c29986b4eef17b5c7764454a3bfe19
Alateon <dc.shoes@hotmail.fr>
2016-03-22 21:09:05 +0100

    ajout du changement de mois

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-choosenMonth=2;
+choosenMonth=5;
+choosenYear=2017;
+
 
 //renvoie le nombre de jours dans un moi d'une annee ex:getDaysInMonth(12,2000);
 function getDaysInMonth(month,year)
 {
 	return (new Date(Date.parse(((month%12)+1).toString() + "/01/" + year)-86400000)).getDate();
 }
+
 function getJours(i)
 {
+	if(i==0){i=1;}
 	switch (i)
 	{
 		case 1:
@@ -33,6 +37,7 @@ function getJours(i)
 	}
 	return day;
 }
+
 function getMonth(i)
 {
 	switch (i)
@@ -76,29 +81,14 @@ function getMonth(i)
 	}
 	return month;
 }
+
 //renvoie le premier jour d'un moi d'une annee/*(1-12),9999*/ ex:getFirstDayInMonth(12,2000);
 function getFirstDayInMonth(whichMonth,Ofyear)
 {
-	// var day
-
-	// if(whichMonth>12 || whichMonth<1 )
-	// {
-	// 	day='Undefined';
-	// 	whichMonth=1;
-	// }
-	// if(day!='Undefined')
-	// {
-	// }
 	getJours((new Date(Ofyear+'-'+whichMonth+'-'+1)).getDay());
-	return Day={nbDay:(new Date(Ofyear+'-'+whichMonth+'-'+1)).getDay(),nameDay:day};
+	if( (new Date(Ofyear+'-'+whichMonth+'-'+1)).getDay()==0 ){ return 7 }
+	else { return (new Date(Ofyear+'-'+whichMonth+'-'+1)).getDay();}
 }
-//le tableau commence Lundi et fini le dimanche, il est statique et il comporte 6 lignes de jours en hauteur il comporte donc 42 jours
-//Le mois sous forme de jour en francais:
-console.log(getFirstDayInMonth(12,9999).nbDay);
-//L'incrementation des jours du mois dans le tableau debutera grace a ce numero de celule :
-console.log(getFirstDayInMonth(03,2016).nameDay);
-//renvoie le nombre de jours dans une annee
-console.log(getDaysInMonth(03,2016));
 
 function createMonthDaysName (i)
 {
@@ -109,14 +99,15 @@ function createMonthDaysName (i)
 		$("#calendrier-app-day-th-container-"+i).text(getJours(i));
 }
 
-function createMonthsDays(nbFirdtDay,nbDaysInCurentMonth,nbDaysInLastMonth)//refactoryser cette fonction separer le table et les tr calendarStructure
+function createMonthsDays(nbFirstDay,nbDaysInCurentMonth,nbDaysInLastMonth)//refactoryser cette fonction separer le table et les tr calendarStructure
 {
 	var tableCell=1;
 	var tableRow=1;
 	var dayInCurentMonth=1;
 	var sevenMonthDaysName=1;
 	var dayInCurentMonthClass="";
-	var dayDisplayed=nbDaysInLastMonth-nbFirdtDay+2;
+	var dayDisplayed=nbDaysInLastMonth-nbFirstDay+2;
+	if(nbDaysInLastMonth-nbFirstDay+2==32){console.log('ezqsdsdfqsdfqsf');}
 
 	var table = document.createElement('table');	
 	table.setAttribute("class","calendrier-app-days-table-container");
@@ -138,18 +129,25 @@ function createMonthsDays(nbFirdtDay,nbDaysInCurentMonth,nbDaysInLastMonth)//ref
 				createMonthDaysName(sevenMonthDaysName);
 				sevenMonthDaysName++;
 			}
+
 			else
 			{
 
-				if(tableCell==nbFirdtDay || dayInCurentMonth>nbDaysInCurentMonth)
+				if(tableCell==nbFirstDay || dayInCurentMonth>nbDaysInCurentMonth)
 				{
 					dayInCurentMonth=1;
-					if(tableCell>27){dayInCurentMonthClass="";
-				}
-				else{
-					dayInCurentMonthClass="test-test-test";}
+					if(tableCell>27)
+					{
+						dayInCurentMonthClass="";
+					}
+					else
+					{
+						dayInCurentMonthClass="test-test-test";
+					}
+
 					dayDisplayed=1;
 				}
+					console.log('testttt'+dayDisplayed);
 				var day = document.createElement('td');
 				day.setAttribute("id","calendrier-app-day-td-container-"+tableCell);
 				day.setAttribute("class","calendrier-app-day-td-container "+dayInCurentMonthClass);
@@ -159,18 +157,70 @@ function createMonthsDays(nbFirdtDay,nbDaysInCurentMonth,nbDaysInLastMonth)//ref
 				tableCell++;
 				dayDisplayed++;
 			}
-			//console.log('dayInCurentMonth='+dayInCurentMonth+' | '+tableCell+'=='+nbFirdtDay +'||'+ a+'=='+nbDaysInCurentMonth);
+			//console.log('dayInCurentMonth='+dayInCurentMonth+' | '+tableCell+'=='+nbFirstDay +'||'+ a+'=='+nbDaysInCurentMonth);
 			dayInCurentMonth++;
 		}
 		tableRow++;
 	}
 }
 
-createMonthsDays(getFirstDayInMonth(choosenMonth,2016).nbDay, getDaysInMonth(choosenMonth,2016), getDaysInMonth(choosenMonth-1,2016));
+function createMonthTitle(choosenMonth)//refactoryser cette fonction separer le table et les tr calendarStructure
+{
+	var titreCalendrierContain = document.createElement("div");
+	titreCalendrierContain.setAttribute("id","calendrier-app-table-header-container");
+	var calendrierAppContainer = document.getElementsByClassName("calendrier-app-container")[0];
+	calendrierAppContainer.insertBefore(titreCalendrierContain, calendrierAppContainer.childNodes[0]);
 
-var titreCalendrierContain = document.createElement("th");calendrierAppContainer;
-titreCalendrierContain.setAttribute("id","calendrier-app-table-title-container");
-titreCalendrierContain.setAttribute("colspan",7);
-var calendrierAppContainer = document.getElementsByClassName("calendrier-app-days-table-container")[0];
-calendrierAppContainer.insertBefore(titreCalendrierContain, calendrierAppContainer.childNodes[0]);
-$("#calendrier-app-table-title-container").text(getMonth(choosenMonth));
+	var leftArrowContainer = document.createElement("div");
+	leftArrowContainer.setAttribute("id","calendrier-app-table-header-left-arrow-container");
+	$("#calendrier-app-table-header-container")[0].appendChild(leftArrowContainer);
+
+	var titreCalendrierContain = document.createElement("div");
+	titreCalendrierContain.setAttribute("id","calendrier-app-table-title-container");
+	$("#calendrier-app-table-header-container")[0].appendChild(titreCalendrierContain);
+
+	var rightArrowContainer = document.createElement("div");
+	rightArrowContainer.setAttribute("id","calendrier-app-table-header-right-arrow-container");
+	$("#calendrier-app-table-header-container")[0].appendChild(rightArrowContainer);
+
+	$("#calendrier-app-table-header-left-arrow-container").text("<");
+	$("#calendrier-app-table-title-container").text(getMonth(choosenMonth)+' ('+choosenMonth+') | '+
+choosenYear);
+	$("#calendrier-app-table-header-right-arrow-container").text(">");
+}
+
+function createCliqueArrows (choosenMonth)
+{
+
+	$("#calendrier-app-table-header-left-arrow-container")[0].addEventListener("click", function (){
+		$('div table').remove();
+		$('div #calendrier-app-table-header-container').remove();
+		choosenMonth--;
+		if(choosenMonth<1){choosenMonth=12;
+choosenYear--}
+		createAll(choosenMonth);
+
+		}, false);	
+
+	$("#calendrier-app-table-header-right-arrow-container")[0].addEventListener("click", function (){
+
+		$('div table').remove();
+		$('div #calendrier-app-table-header-container').remove();
+
+		choosenMonth++;
+		if(choosenMonth>12){choosenMonth=1;
+choosenYear++;}
+
+		createAll(choosenMonth);
+
+	}, false);
+}
+
+function createAll (choosenMonth)
+{
+	createMonthsDays(getFirstDayInMonth(choosenMonth,choosenYear), getDaysInMonth(choosenMonth,choosenYear), getDaysInMonth(choosenMonth-1,choosenYear));
+	createMonthTitle(choosenMonth);
+	createCliqueArrows(choosenMonth);
+}
+
+createAll(choosenMonth);
